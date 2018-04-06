2e3c09b036978c9b6cb5cd30b350d8b65d8fcc31
huggla <henrik.uggla@kristianstad.se>
2018-04-06 08:58:55 +0200

    Update and rename initdb/020.postgres.sh to initdb/sql/020.postgres.sql

--- initdb/020.postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "CREATE EXTENSION IF NOT EXISTS postgis;" >> "$sql_file"
-echo "CREATE EXTENSION IF NOT EXISTS postgis_topology;" >> "$sql_file"
-echo "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;" >> "$sql_file"
-echo "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;" >> "$sql_file"
-echo "CREATE DATABASE template_postgis;" >> "$sql_file"
-echo "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';" >> "$sql_file"
